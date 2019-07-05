b47da0fd52cd9e4727b851bf32ae022dd8320919
Pol d'Avezac <pol.davezac@depixus.com>
2019-07-05 08:14:15 +0200

    adding better means for extracting colors from the bokeh API

---
+++ tests/core/test_view.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"Testing views"
+from typing       import Iterator
+from bokeh.colors import named    as _bkclr
+from bokeh        import palettes as _palette
+from view.colors  import tohex
+
+class _dico:
+    def __init__(self, *vals, **kwa):
+        self.__dict__.update(*vals, **kwa)
+
+    def items(self):
+        "get items"
+        return self.__dict__.items()
+
+def test_colors():
+    "test color retrieval"
+    assert tohex('red') == _bkclr.red.to_hex()
+    assert tohex('RdYlGn9') == getattr(_palette, 'RdYlGn9')
+    assert tohex(['brown']) == [_bkclr.brown.to_hex()]
+    assert tohex({'brown'}) == {_bkclr.brown.to_hex()}
+    assert tohex(('brown',)) == (_bkclr.brown.to_hex(),)
+    assert isinstance(tohex(iter(('brown',))), Iterator)
+    assert isinstance(tohex(_dico()), _dico)
+    assert dict(tohex(_dico(x = 'lightblue')).items()) == {'x': _bkclr.lightblue.to_hex()}
+
+if __name__ == '__main__':
+    test_colors()
--- view/colors.py
+++ view/colors.py
@@ -1,15 +1,40 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 "Cycles plot view for cleaning data"
-from typing import Dict, Any, Union
+from typing         import (
+    Dict, Any, Union, List, Tuple, Set, Iterator, overload, cast
+)
+from bokeh.colors   import named    as _bkclr
+from bokeh          import palettes as _palette
 
-try:
-    from    bokeh.colors   import named as _bkclr
-except ImportError:
-    import  bokeh.colors   as _bkclr
+@overload
+def tohex(clr:Dict[Any,str]) -> Dict[Any, str]:
+    "return a dictionnary of colors in hex format"
+@overload
+def tohex(clr:Tuple[str]) -> Tuple[str]:
+    "return an iteration of colors in hex format"
+@overload
+def tohex(clr:List[str]) -> List[str]:
+    "return an iteration of colors in hex format"
+@overload
+def tohex(clr:Set[str]) -> Set[str]:
+    "return an iteration of colors in hex format"
+@overload
+def tohex(clr:str) -> Union[str, List[str]]:
+    "return either the palette or the hex color associated to a name"
 
-def tohex(clr:Union[Dict[Any,str], str]):
+def tohex(clr:Union[Dict[Any,str], List[str], Tuple[str], Set[str], str]):
     "return the hex value"
-    if isinstance(clr, dict):
-        return {i: tohex(j) for i, j in clr.items()}
-    return clr if len(clr) > 0 and clr[0] == '#' else getattr(_bkclr, clr).to_hex()
+    return (
+        type(cast(dict, clr))((i, tohex(j)) for i, j in getattr(clr, 'items')())
+        if callable(getattr(clr, 'items', None)) else
+
+        (tohex(j) for j in clr)         if isinstance(clr, Iterator)   else
+
+        type(cast(list, clr))(tohex(j) for j in clr)
+        if not isinstance(clr, str)   else
+
+        clr                             if len(clr) and clr[0] == '#' else
+        getattr(_palette, clr)          if hasattr(_palette, clr)     else
+        getattr(_bkclr, clr).to_hex()
+    )
